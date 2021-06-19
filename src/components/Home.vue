70b4265d9024b7579aa9c0de54931687e650307f
adrien <aguemy@student.42.fr>
2021-06-19 23:50:03 +0200

    using api.aoe2-stats.com instead of heroku

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Hello there</h1>
-    <router-link v-bind:to="{ name: 'Posts' }">Go to posts</router-link> 
+    <router-link v-bind:to="{ name: 'Posts' }">Go to posts</router-link>
   </div>
 </template>
 
--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `https://api-aoe-stats-com.herokuapp.com`
+    baseURL: `https://api.aoe2-stats.com`
   })
 }
